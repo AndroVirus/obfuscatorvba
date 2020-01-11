6b93ebc9ee60b3f254abedc93432c014087d993b
Gray Programmerz <hackrefisher@gmail.com>
2020-01-11 13:54:08 +0500

    Add files via upload

---
+++ installer.bat
@@ -0,0 +1,10 @@
+@echo off
+color 0a
+echo          +++++RUN AS ADMINISTRATOR TO AVOID ERROR+++++
+echo          +++++INSTALLING TERMCOLOR+++++
+pip3 install termcolor
+echo          +++++INSTALLING PYGMENTS+++++
+pip3 install pygments
+
+echo           +++++OPERATION COMPLETED !!!+++++
+pause
